c5b2bf049c5183014c6ec4c5f3cd96166b5ee583
Kshitij <65331304+kshitij1235@users.noreply.github.com>
2021-10-22 23:14:39 +0530

    Pushed working exe file

--- windows10 activation/windows_activation.py
+++ windows10 activation/windows_activation.py
@@ -1,13 +1,18 @@
-# pakages 
-
-import tkinter as tk
+"""windows actvation pakages"""
+import threading
 from tkinter import ttk
-from tkinter import *
-from PIL import ImageTk, Image  
+import os
+import tkinter
+from PIL import ImageTk, Image
 
-import os 
+
+def unlock():
+    """allows threading that helps with perforamnce"""
+    threading.Thread(target=commands).start()
+
 
 def commands():
+    """commands that help windows activation"""
     pb['value'] += 20
     os.system("c:")
     pb['value'] += 10
@@ -21,51 +26,46 @@ def commands():
     pb['value'] += 20
 
 
-
 if __name__ == "__main__":
 
-
-# gui code 
+    # gui code
 
     # root window
-    root = tk.Tk()
+    root = tkinter.Tk()
     root.config(bg="#218DBF")
-     #TITLE
+    # TITLE
     root.title("Windows Activation")
     try:
-        root.iconbitmap('icon.ico')
+        root.iconbitmap('./icon.ico')
     except:
-        print("cant load icon")
+        pass
 
-    #SCREENSIZE
+    # SCREENSIZE
     root.geometry("300x230")
-    root.minsize(300,230)
-    root.maxsize(300,230)
+    root.minsize(300, 230)
+    root.maxsize(300, 230)
 
+    # menu widigits
+    try:
 
-    # menu widigits 
+        image1 = Image.open("icon.ico")
+        test = ImageTk.PhotoImage(image1)
+        label1 = tkinter.Label(image=test, bg="#218DBF")
+        label1.image = test
+        # Position image
+        label1.place(x=70, y=1)
+    except:
+        pass
 
-    image1 = Image.open("icon.ico")
-    test = ImageTk.PhotoImage(image1)
-    label1 = tk.Label(image=test,bg="#218DBF")
-    label1.image = test
+    lab = tkinter.Label(
+        root, text="! make sure you open this software as admin", bg="lightblue")
+    lab.place(y=120, x=10)
 
-    # Position image
-    label1.place(x=70, y=1)
+    pb = ttk.Progressbar(root, orient='horizontal',
+                         mode='determinate', length=280)
+    pb.place(x=10, y=170)
 
-
-    lab=tk.Label(root,text="! make sure you open this software as admin",bg="lightblue")
-    lab.place(y=120,x=10)
-
-    pb = ttk.Progressbar(root,orient='horizontal',mode='determinate',length=280)
-    pb.place(x=10,y=170)
-
-    B = ttk.Button(root, text ="Activate",command=commands) 
+    B = tkinter.Button(root, text="Activate", command=unlock)
     B.place(x=100, y=200)
-
-
-
-
-
 
     root.mainloop()
