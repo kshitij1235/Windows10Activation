82ae96f46966594c3b7086ab6db5dcf6549489b4
Kshitij <65331304+kshitij1235@users.noreply.github.com>
2021-10-24 23:01:21 +0530

    Updated

--- windows10 activation/windows_activation.py
+++ windows10 activation/windows_activation.py
@@ -8,6 +8,7 @@ from PIL import ImageTk, Image
 
 def unlock():
     """allows threading that helps with perforamnce"""
+    pb.place(x=10, y=170)
     threading.Thread(target=commands).start()
 
 
@@ -21,49 +22,37 @@ def commands():
     os.system("slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX")
     pb['value'] += 10
     os.system("slmgr.vbs /skms kms.lotro.cc")
-    pb['value'] += 10
-    os.system("slmgr.vbs /ato")
     pb['value'] += 20
+    os.system("slmgr.vbs /ato")
 
 
 if __name__ == "__main__":
 
-    # gui code
-
-    # root window
     root = tkinter.Tk()
     root.config(bg="#218DBF")
-    # TITLE
     root.title("Windows Activation")
-    try:
-        root.iconbitmap('./icon.ico')
-    except:
-        pass
 
-    # SCREENSIZE
+    try:root.iconbitmap('./icon.ico')
+    except:pass
+
     root.geometry("300x230")
     root.minsize(300, 230)
     root.maxsize(300, 230)
 
-    # menu widigits
     try:
 
         image1 = Image.open("icon.ico")
         test = ImageTk.PhotoImage(image1)
         label1 = tkinter.Label(image=test, bg="#218DBF")
         label1.image = test
-        # Position image
         label1.place(x=70, y=1)
-    except:
-        pass
+    except:pass
 
-    lab = tkinter.Label(
-        root, text="! make sure you open this software as admin", bg="lightblue")
+    lab = tkinter.Label(root, text="! make sure you open this software as admin", bg="lightblue")
     lab.place(y=120, x=10)
 
-    pb = ttk.Progressbar(root, orient='horizontal',
-                         mode='determinate', length=280)
-    pb.place(x=10, y=170)
+    pb = ttk.Progressbar(root, orient='horizontal',mode='determinate', length=280)
+ 
 
     B = tkinter.Button(root, text="Activate", command=unlock)
     B.place(x=100, y=200)
